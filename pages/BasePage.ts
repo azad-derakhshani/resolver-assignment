f0fe7f99bae49445c43757a2b0495918ce50024b
azad-derakhshani <derakhshani.azad@gmail.com>
2023-01-29 21:49:19 -0500

    start implementing BasePage

---
+++ pages/BasePage.ts
@@ -0,0 +1,26 @@
+import {Builder, Capabilities, By, Key, until} from 'selenium-webdriver';
+import { Chance } from 'chance';
+
+export class BasePage {
+
+    //Tools
+    driver: Builder;
+    capabilities: Capabilities;
+    chance: Chance;
+
+    constructor() {
+
+        //Tools
+        this.driver = new Builder().forBrowser('chrome').build();
+        this.capabilities = new Capabilities();
+        this.chance = new Chance();
+
+    }
+
+    async goTo() {
+        await this.driver.manage().window().maximize();
+        await this.driver.get('QE-index.html');
+        await this.driver.manage().setTimeouts({implicit: 8 });
+    }
+
+}
