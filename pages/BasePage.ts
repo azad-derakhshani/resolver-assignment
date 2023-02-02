5d8147da4ddf78af791a1f2684995ebb4b9559ba
azad-derakhshani <derakhshani.azad@gmail.com>
2023-02-01 21:52:39 -0500

    add element methods to BasePage

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -22,4 +22,33 @@ export class BasePage {
         await this.driver.manage().setTimeouts({implicit: 8 });
     }
 
+    async elementHasText(element, text) {
+        await expect(element).toHaveTextContent(text);
+    }
+
+    async elementHasValue(element, value) {
+        await expect(element).toHaveValue(value)
+    }
+
+    async selectOptionFromDropdown(dropdown, optionIndex) {
+        let dropdownOption = this.driver.findElement(By.text(`Option ${optionIndex}`))
+        await dropdown.click();
+        await dropdownOption.click()
+    }
+
+    async checkButtonStatus(button, expectedStatus){
+        if (expectedStatus = 'enabled') {
+            await expect(button).toBeEnabled();
+        }
+        else if (expectedStatus = 'disabled') {
+            await expect(button).toBeDisabled();
+        }
+    }
+
+    async getCellValueInTable(row, column) {
+        let cell = await this.driver.findElement(By.xpath(`//table[contains(@class, "table")]//tr[${row + 1}]//td[${column + 1}]`));
+        //row and column index start at 0, but Xpath element indexes start at 1
+        return cell.getText();
+    }
+
 }
